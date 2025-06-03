7a504bcc1743096e78e39440c822908394800b23
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-04 09:07:59 +1000

    added polly and immediate retry policy

---
+++ RequestService/Controllers/RequestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RequestService.Policies;
+
+namespace RequestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly ClientPolicy clientPolicy;
+
+        public RequestController(ClientPolicy clientPolicy)
+        {
+            this.clientPolicy = clientPolicy;
+        }
+
+        // GET api/response/100
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> MakeRequest(int id)
+        {
+            var client = new HttpClient();
+            //var response = await client.GetAsync($"https://localhost:7297/api/response/{id}");
+
+            var response = await clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+
+            if (response.IsSuccessStatusCode) 
+            {
+                Console.WriteLine("----> ResponseService returned SUCCESS");
+                return StatusCode(StatusCodes.Status200OK, id);
+            }
+            Console.WriteLine("----> ResponseService returned FAILURE");
+            return StatusCode(StatusCodes.Status500InternalServerError, id);
+        }
+    }
+}
---
+++ RequestService/Policies/ClientPolicy.cs
@@ -0,0 +1,16 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RequestService.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+
+        public ClientPolicy()
+        {
+            // if the response is not a http successcode, retry for another 5 attempts
+            ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode).RetryAsync(5);
+        }
+    }
+}
