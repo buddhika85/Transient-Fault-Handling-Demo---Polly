2430ea1fd1c250de30307dec9e2c23e5308ccd69
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-03 17:33:45 +1000

    get reponse end point

---
+++ ResponseAPI/Controllers/ResponseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+        // GET api/response/100
+        // if you pass 100 or more you have more chance of getting Ok response
+        // if you pass something like 1 you have high chance of getting 500 internal server error/ failure
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult GetResponse(int id)
+        {
+            var random = new Random();
+            var randomNum = random.Next(1, 101);
+            if (randomNum >= id)
+            {
+                Console.WriteLine("----> Failure - Generated 500");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            Console.WriteLine("----> Success - Generated 200");
+            return StatusCode(StatusCodes.Status200OK, $"{id} >= random {randomNum} == true");
+        }
+    }
+}
