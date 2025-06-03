8665bc8f7cec859b03c4ef5dbc000387a5b5b861
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-04 09:32:52 +1000

    added HTTP client factory

--- RequestService/Controllers/RequestController.cs
+++ RequestService/Controllers/RequestController.cs
@@ -8,30 +8,60 @@ namespace RequestService.Controllers
     public class RequestController : ControllerBase
     {
         private readonly ClientPolicy clientPolicy;
+        private readonly IHttpClientFactory clientFactory;
 
-        public RequestController(ClientPolicy clientPolicy)
+        public RequestController(ClientPolicy clientPolicy, IHttpClientFactory clientFactory)
         {
             this.clientPolicy = clientPolicy;
+            this.clientFactory = clientFactory;
         }
+
+        //// GET api/response/100
+        //[Route("{id}")]
+        //[HttpGet]
+        //public async Task<IActionResult> MakeRequest(int id)
+        //{
+        //    var client = new HttpClient();
+        //    //var response = await client.GetAsync($"https://localhost:7297/api/response/{id}");        // run once - no polly
+
+        //    // 1 using polly, if failed, retry 5 times immediately 
+        //    //var response = await clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+
+        //    // 2 using polly, if failed, retry 5 times, 3 seconds waits in between 
+        //    //var response = await clientPolicy.LinearHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+
+        //    // 3 using polly, if failed, retry 5 times, 3 seconds waits in between 
+        //    var response = await clientPolicy.ExponetialHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+
+        //    if (response.IsSuccessStatusCode) 
+        //    {
+        //        Console.WriteLine("----> ResponseService returned SUCCESS");
+        //        return StatusCode(StatusCodes.Status200OK, id);
+        //    }
+        //    Console.WriteLine("----> ResponseService returned FAILURE");
+        //    return StatusCode(StatusCodes.Status500InternalServerError, id);
+        //}
+
+
 
         // GET api/response/100
         [Route("{id}")]
         [HttpGet]
         public async Task<IActionResult> MakeRequest(int id)
         {
-            var client = new HttpClient();
+            var client = clientFactory.CreateClient();
             //var response = await client.GetAsync($"https://localhost:7297/api/response/{id}");        // run once - no polly
 
             // 1 using polly, if failed, retry 5 times immediately 
-            //var response = await clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+            var response = await clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
 
             // 2 using polly, if failed, retry 5 times, 3 seconds waits in between 
             //var response = await clientPolicy.LinearHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
 
             // 3 using polly, if failed, retry 5 times, 3 seconds waits in between 
-            var response = await clientPolicy.ExponetialHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+            //var response = await clientPolicy.ExponetialHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
 
-            if (response.IsSuccessStatusCode) 
+            if (response.IsSuccessStatusCode)
             {
                 Console.WriteLine("----> ResponseService returned SUCCESS");
                 return StatusCode(StatusCodes.Status200OK, id);
--- RequestService/Program.cs
+++ RequestService/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // POLLY fault retry policies 
+builder.Services.AddHttpClient();                       // we need HTTP client factory 
 builder.Services.AddSingleton<ClientPolicy>();
 
 builder.Services.AddControllers();
