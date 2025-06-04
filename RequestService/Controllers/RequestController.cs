f50abaeea425703238ac03a8505162fa3c561f56
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-04 10:29:47 +1000

    added circuite breaker

--- RequestService/Controllers/RequestController.cs
+++ RequestService/Controllers/RequestController.cs
@@ -16,7 +16,7 @@ namespace RequestService.Controllers
             this.clientFactory = clientFactory;
         }
 
-        //// GET api/response/100
+        //// GET api/response/30
         //[Route("{id}")]
         //[HttpGet]
         //public async Task<IActionResult> MakeRequest(int id)
@@ -44,22 +44,45 @@ namespace RequestService.Controllers
 
 
         // using HTTP client factory to create HTTP Client
-        // GET api/response/100
-        [Route("{id}")]
+        // GET api/response/MakeRequestRetry/30
+        [Route("MakeRequestRetry/{id}")]
         [HttpGet]
-        public async Task<IActionResult> MakeRequest(int id)
+        public async Task<IActionResult> MakeRequestRetry(int id)
         {
             var client = clientFactory.CreateClient("PollyPolicyCleint");
-            //var response = await client.GetAsync($"https://localhost:7297/api/response/{id}");        // run once - no polly
+            
 
-            // 1 using polly, if failed, retry 5 times immediately 
-            var response = await clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+            // using polly retries configured in Program.cs
+            var response = await client.GetAsync($"https://localhost:7297/api/response/{id}");
 
-            // 2 using polly, if failed, retry 5 times, 3 seconds waits in between 
-            //var response = await clientPolicy.LinearHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+          
 
-            // 3 using polly, if failed, retry 5 times, 3 seconds waits in between 
-            //var response = await clientPolicy.ExponetialHttpRetry.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("----> ResponseService returned SUCCESS");
+                return StatusCode(StatusCodes.Status200OK, id);
+            }
+            Console.WriteLine("----> ResponseService returned FAILURE");
+            return StatusCode(StatusCodes.Status500InternalServerError, id);
+        }
+
+
+
+        // GET api/response/MakeRequestCircuiteBreaker/30
+        [Route("MakeRequestCircuiteBreaker/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> MakeRequestCircuiteBreaker(int id)
+        {
+            var client = new HttpClient();
+           
+
+            // using polly, if failed
+            // do immediate 10 requests
+            // if all fails then take a break for  5 seconds
+            // do immediate 10 requests 
+            // ..
+            var response = await clientPolicy.CircuiteBreakerHttpPolicy.ExecuteAsync(() => client.GetAsync($"https://localhost:7297/api/response/{id}"));
+
 
             if (response.IsSuccessStatusCode)
             {
--- RequestService/Policies/ClientPolicy.cs
+++ RequestService/Policies/ClientPolicy.cs
@@ -1,5 +1,8 @@
 ﻿using Polly;
 using Polly.Retry;
+using System.Diagnostics.Metrics;
+using System;
+using Polly.CircuitBreaker;
 
 namespace RequestService.Policies
 {
@@ -10,19 +13,44 @@ namespace RequestService.Policies
 
         public AsyncRetryPolicy<HttpResponseMessage> ExponetialHttpRetry { get; }
 
+
+        public AsyncPolicy<HttpResponseMessage> CircuiteBreakerHttpPolicy { get; }
+
         public ClientPolicy()
         {
             // if the response is not a http successcode,
-            
+
+            #region RETRY_POLICIES
             // retry for another 5 attempts
             ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode).RetryAsync(5);
 
             // retry for another 5 attempts, while waiting 3 seconds in between
             LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode).WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
 
+            // can use exponential backoff to gradually increase wait time between retries.
             // retry for another 5 attempts, while waiting 1 seconds first, 4 seconeds second, 9 seconds third
             ExponetialHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode).WaitAndRetryAsync(5, retryAttempt => 
                                                             TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+            #endregion
+
+            #region CIRCUITE_BREAKER
+            // Prevents repeated failures by temporarily blocking requests after a threshold is reached.
+            // do immediate 10 requests
+            // if all fails then take a break for  5 seconds
+            // do immediate 10 requests 
+            // ..
+
+            var retryPolicy = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                            .RetryAsync(10); // Retries 10 times
+
+            // Breaks after 10 failures, and rests for 5 seconds
+            var circuiteBreakerPolicy = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                .CircuitBreakerAsync(handledEventsAllowedBeforeBreaking: 10, durationOfBreak: TimeSpan.FromSeconds(5));
+
+            CircuiteBreakerHttpPolicy = Policy.WrapAsync(retryPolicy, circuiteBreakerPolicy);
+
+
+            #endregion region
         }
     }
 }
